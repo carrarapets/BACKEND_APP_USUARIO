284ebd1fa92ff75cde1157de5c00a7ef8225d59a
carrapets <carrapets@gmail.com>
2022-09-18 21:45:19 -0300

    commit v20 - voltando para o heroku

--- src/app/domain/config/index.js
+++ src/app/domain/config/index.js
@@ -3,15 +3,15 @@ const {logger} = require("../logger");
 dotenv.config()
 
 const config = {
-    ENV: process.env.ENV,
-    EXPRESS_PORT:parseInt(process.env.EXPRESS_PORT, 10)  ,
-    SECRET: process.env.SECRET,
-    API_URL: process.env.API_URL,
-    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
-    POSTGRES_USER: process.env.POSTGRES_USER,
-    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-    POSTGRES_HOST: process.env.POSTGRES_HOST,
-    POSTGRES_PORT: process.env.POSTGRES_PORT
+    ENV: PRODUCTION,
+    EXPRESS_PORT:5432,
+    SECRET: SECRET,
+    API_URL: "postgres://hhkaraiurohita:92aa7f1eb10c78458370cb8ee5256f28a2732f3a57f79475a85e7cc25a3a0fa6@ec2-34-200-205-45.compute-1.amazonaws.com:5432/d3q042tprckimk",
+    POSTGRES_DATABASE: "d3q042tprckimk",
+    POSTGRES_USER: "hhkaraiurohita",
+    POSTGRES_PASSWORD: "92aa7f1eb10c78458370cb8ee5256f28a2732f3a57f79475a85e7cc25a3a0fa6",
+    POSTGRES_HOST: "ec2-34-200-205-45.compute-1.amazonaws.com",
+    POSTGRES_PORT: 5432
 
     
 }
